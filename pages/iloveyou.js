810963ac9be3fae181364321ec54003bbcef04a3
Maxime TFT <mkubik@tftlabs.com>
2022-02-14 17:55:10 +0100

    💕 and done

--- pages/iloveyou.js
+++ pages/iloveyou.js
@@ -1,27 +1,31 @@
 import Link from "next/link";
+import { useState } from "react";
 
-const iloveyou = () => {
+export default function Iloveyou() {
+  const isLoading = useState(false);
+
+  const mounted = () => {
+    setTimeout(() => (this.isLoading = false), 3000); //Note: When interacting with a state manager(e.g. Vuex), remove this hook and let the flag to be filled from a state getter.
+  };
+
   return (
-    <div className="h-screen bg-gradient-to-r from-rose-700 to-pink-600">
-      <div className="flex flex-col items-center justify-center">
+      <div className="h-screen bg-gradient-to-r from-rose-700 to-pink-600 flex flex-col items-center justify-center">
 
-        {/* Image */}
-        <div className="pt-12 my-10 animate-bounce">
+        {/* Image coeur */}
+        <div className="pt-16 animate-bounce">
           <img alt="heart" className="mx-auto w-1/2" src="./hearth.png"></img>
         </div>
 
-        <div className="text-white text-3xl font-bold text-center px-8">
-          <p>Joyeuse Saint Valentin 2022 🌷💕</p>
+        {/* Text Content */}
+        <div className="animate-pulse text-white text-4xl font-bold text-center px-8">
+          <p>Joyeuse Saint-Valentin 2022</p>
+          <p className="pt-2">💕💕💕</p>
+
         </div>
-
-        {/* Content */}
-        <div className="content-center">
-         
-
+        <div className=" text-white text-2xl font-bold text-center px-8 py-24">
+          <p>Je t&apos;aime pour toute la vie.</p>
+          <p>Dispo pour un restaurant dans la semaine? 😇</p>
         </div>
       </div>
-    </div>
   );
 }
-
-export default iloveyou;
--- pages/index.js
+++ pages/index.js
@@ -5,29 +5,26 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-gradient-to-r from-rose-700 to-pink-600">
-      <div className="flex flex-col items-center justify-center">
+    <div className="h-screen bg-gradient-to-r from-rose-700 to-pink-600 flex flex-col items-center justify-center">
+      {/* Image */}
+      <div className="pt-16 animate-bounce">
+        <img alt="heart" className="mx-auto w-1/2" src="./letter.png"></img>
+      </div>
 
-        {/* Image */}
-        <div className="pt-16 my-10 animate-bounce">
-          <img alt="heart" className="mx-auto w-1/2" src="./hearth.png"></img>
-        </div>
+      <div className="text-white text-3xl font-bold text-center px-8">
+        <p>Une surprise t&apos;attends mon petit cul... 🌷</p>
+      </div>
 
-        <div className="text-white text-3xl font-bold text-center px-8">
-          <p>Une surprise t&apos;attends mon petit cul... 🌷</p>
-        </div>
-
-        {/* Button */}
-        <div className="content-center">
-          <Link href="/iloveyou">
-            <a
-              type="button"
-              className="mx-auto my-16 text-center animate-pulse items-center px-6 py-3 text-base font-medium rounded-md shadow-sm text-white bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 hover:bg-gradient-to-r hover:from-rose-500 hover:via-fuchsia-600 hover:to-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white"
-            >
-              Voir késséssé la surprise 💌
-            </a>
-          </Link>
-        </div>
+      {/* Button */}
+      <div className="content-center">
+        <Link href="/iloveyou">
+          <a
+            type="button"
+            className="transition duration-300 ease-in-out mx-auto my-16 text-center animate-pulse items-center px-6 py-3 text-base font-medium rounded-md shadow-sm text-white bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 hover:bg-gradient-to-r hover:from-rose-500 hover:via-fuchsia-600 hover:to-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white"
+          >
+            Voir késséssé la surprise 💌
+          </a>
+        </Link>
       </div>
     </div>
   );
